67eb41dd887adb0c656644393b1d67c38bb2582a
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2021-03-08 11:38:13 +0300

    Adds cameras and instancing;

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::{camera::*, instance::*, texture::*};
+use cgmath::prelude::*;
 use futures::executor::block_on;
 use wgpu::util::DeviceExt;
 use winit::{
@@ -5,6 +7,9 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
+
+mod camera;
+mod instance;
 mod texture;
 
 const VERTICES: &[Vertex] = &[
@@ -31,6 +36,14 @@ const VERTICES: &[Vertex] = &[
 ];
 
 const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+
+const NUM_INSTANCES_PER_ROW: u32 = 10;
+const NUM_INSTANCES: u32 = NUM_INSTANCES_PER_ROW * NUM_INSTANCES_PER_ROW;
+const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(
+    NUM_INSTANCES_PER_ROW as f32 * 0.5,
+    0.0,
+    NUM_INSTANCES_PER_ROW as f32 * 0.5,
+);
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
@@ -59,6 +72,29 @@ impl Vertex {
     }
 }
 
+// We need this for Rust to store our data correctly for the shaders
+#[repr(C)]
+// This is so we can store this in a buffer
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct Uniforms {
+    // We can't use cgmath with bytemuck directly so we'll have
+    // to convert the Matrix4 into a 4x4 f32 array
+    view_proj: [[f32; 4]; 4],
+}
+
+impl Uniforms {
+    fn new() -> Self {
+        use cgmath::SquareMatrix;
+        Self {
+            view_proj: cgmath::Matrix4::identity().into(),
+        }
+    }
+
+    fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.build_view_projection_matrix().into();
+    }
+}
+
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
@@ -75,7 +111,14 @@ struct State {
     num_indices: u32,
     diffuse_bind_groups: [wgpu::BindGroup; 2],
     current_diffuse_bind_group: usize,
-    diffuse_texture: texture::Texture,
+    diffuse_texture: Texture,
+    uniforms: Uniforms,
+    uniform_buffer: wgpu::Buffer,
+    uniform_bind_group: wgpu::BindGroup,
+    camera: Camera,
+    camera_controller: CameraController,
+    instances: Vec<Instance>,
+    instance_buffer: wgpu::Buffer,
 }
 
 impl State {
@@ -143,52 +186,20 @@ impl State {
                 label: Some("texture_bind_group_layout"),
             });
 
-        let diffuse_texture_1 = texture::Texture::from_bytes(
-            &device,
-            &queue,
-            include_bytes!("happy-tree.png"),
-            "happy-tree.png",
-        )
-        .unwrap();
-
-        let diffuse_bind_group_1 = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &texture_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
+        let uniform_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[wgpu::BindGroupLayoutEntry {
                     binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&diffuse_texture_1.view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&diffuse_texture_1.sampler),
-                },
-            ],
-            label: Some("diffuse_bind_group"),
-        });
-
-        let diffuse_texture_2 = texture::Texture::from_bytes(
-            &device,
-            &queue,
-            // include_bytes!("sad-tree.png"),
-            include_bytes!("happy-tree.png"),
-            "sad-tree.png",
-        )
-        .unwrap();
-
-        let diffuse_bind_group_2 = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &texture_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&diffuse_texture_2.view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&diffuse_texture_2.sampler),
-                },
-            ],
-            label: Some("diffuse_bind_group"),
-        });
+                    visibility: wgpu::ShaderStage::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+                label: Some("uniform_bind_group_layout"),
+            });
 
         let render_pipeline_1 = {
             let vs_module = device.create_shader_module(&wgpu::include_spirv!("shader.vert.spv"));
@@ -197,7 +208,7 @@ impl State {
             let render_pipeline_layout =
                 device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                     label: Some("Render Pipeline Layout"),
-                    bind_group_layouts: &[&texture_bind_group_layout],
+                    bind_group_layouts: &[&texture_bind_group_layout, &uniform_bind_group_layout],
                     push_constant_ranges: &[],
                 });
 
@@ -207,7 +218,7 @@ impl State {
                 vertex: wgpu::VertexState {
                     module: &vs_module,
                     entry_point: "main",
-                    buffers: &[Vertex::desc()],
+                    buffers: &[Vertex::desc(), InstanceRaw::desc()],
                 },
                 fragment: Some(wgpu::FragmentState {
                     // 3.
@@ -238,6 +249,53 @@ impl State {
             })
         };
 
+        let diffuse_texture_1 = Texture::from_bytes(
+            &device,
+            &queue,
+            include_bytes!("happy-tree.png"),
+            "happy-tree.png",
+        )
+        .unwrap();
+
+        let diffuse_bind_group_1 = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &texture_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&diffuse_texture_1.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&diffuse_texture_1.sampler),
+                },
+            ],
+            label: Some("diffuse_bind_group"),
+        });
+
+        let diffuse_texture_2 = Texture::from_bytes(
+            &device,
+            &queue,
+            // include_bytes!("sad-tree.png"),
+            include_bytes!("happy-tree.png"),
+            "sad-tree.png",
+        )
+        .unwrap();
+
+        let diffuse_bind_group_2 = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &texture_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&diffuse_texture_2.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&diffuse_texture_2.sampler),
+                },
+            ],
+            label: Some("diffuse_bind_group"),
+        });
+
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
             contents: bytemuck::cast_slice(VERTICES),
@@ -249,6 +307,74 @@ impl State {
             usage: wgpu::BufferUsage::INDEX,
         });
         let num_indices = INDICES.len() as u32;
+
+        let camera = Camera {
+            // position the camera one unit up and 2 units back
+            // +z is out of the screen
+            eye: (0.0, 1.0, 2.0).into(),
+            // have it look at the origin
+            target: (0.0, 0.0, 0.0).into(),
+            // which way is "up"
+            up: cgmath::Vector3::unit_y(),
+            aspect: sc_desc.width as f32 / sc_desc.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
+        let mut uniforms = Uniforms::new();
+        uniforms.update_view_proj(&camera);
+
+        let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Uniform Buffer"),
+            contents: bytemuck::cast_slice(&[uniforms]),
+            usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
+        });
+
+        let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &uniform_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: uniform_buffer.as_entire_binding(),
+            }],
+            label: Some("uniform_bind_group"),
+        });
+        let camera_controller = CameraController::new(0.2);
+
+        let instances = (0..NUM_INSTANCES_PER_ROW)
+            .flat_map(|z| {
+                (0..NUM_INSTANCES_PER_ROW).map(move |x| {
+                    let position = cgmath::Vector3 {
+                        x: x as f32,
+                        y: 0.0,
+                        z: z as f32,
+                    } - INSTANCE_DISPLACEMENT;
+
+                    let rotation = if cgmath::Zero::is_zero(&position) {
+                        // this is needed so an object at (0, 0, 0) won't get scaled to zero
+                        // as Quaternions can effect scale if they're not created correctly
+                        cgmath::Quaternion::from_axis_angle(
+                            cgmath::Vector3::unit_z(),
+                            cgmath::Deg(0.0),
+                        )
+                    } else {
+                        cgmath::Quaternion::from_axis_angle(
+                            position.clone().normalize(),
+                            cgmath::Deg(45.0),
+                        )
+                    };
+
+                    Instance { position, rotation }
+                })
+            })
+            .collect::<Vec<_>>();
+
+        let instance_data = instances.iter().map(Instance::to_raw).collect::<Vec<_>>();
+        let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Instance Buffer"),
+            contents: bytemuck::cast_slice(&instance_data),
+            usage: wgpu::BufferUsage::VERTEX,
+        });
+
         Self {
             surface,
             device,
@@ -271,6 +397,13 @@ impl State {
             diffuse_texture: diffuse_texture_1,
             diffuse_bind_groups: [diffuse_bind_group_1, diffuse_bind_group_2],
             current_diffuse_bind_group: 0,
+            camera,
+            uniform_bind_group,
+            uniforms,
+            uniform_buffer,
+            camera_controller,
+            instances,
+            instance_buffer,
         }
     }
 
@@ -282,50 +415,60 @@ impl State {
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        match event {
-            WindowEvent::CursorEntered { .. } => {
-                self.mouse_in_window = true;
-                true
-            }
-            WindowEvent::CursorLeft { .. } => {
-                self.mouse_in_window = false;
-                true
-            }
-            WindowEvent::CursorMoved { position, .. } => {
-                if self.mouse_in_window {
-                    self.clear_color = wgpu::Color {
-                        r: position.x / self.size.width as f64,
-                        g: position.y / self.size.height as f64,
-                        b: 0.3,
-                        a: 1.0,
-                    };
-                    /*  println!(
-                        "mouse moved: x: {} y: {} color: {:?} size: {:?}",
-                        position.x, position.y, self.clear_color, self.size
-                    ); */
-                }
-                true
-            }
-            WindowEvent::KeyboardInput { input, .. } => match input {
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Space),
-                    ..
-                } => {
-                    self.current_pipeline_index =
-                        (self.current_pipeline_index + 1) % self.render_pipelines.len();
-                    self.current_diffuse_bind_group =
-                        (self.current_diffuse_bind_group + 1) % self.diffuse_bind_groups.len();
+        if !self.camera_controller.process_events(event) {
+            match event {
+                WindowEvent::CursorEntered { .. } => {
+                    self.mouse_in_window = true;
                     true
                 }
+                WindowEvent::CursorLeft { .. } => {
+                    self.mouse_in_window = false;
+                    true
+                }
+                WindowEvent::CursorMoved { position, .. } => {
+                    if self.mouse_in_window {
+                        self.clear_color = wgpu::Color {
+                            r: position.x / self.size.width as f64,
+                            g: position.y / self.size.height as f64,
+                            b: 0.3,
+                            a: 1.0,
+                        };
+                        /*  println!(
+                            "mouse moved: x: {} y: {} color: {:?} size: {:?}",
+                            position.x, position.y, self.clear_color, self.size
+                        ); */
+                    }
+                    true
+                }
+                WindowEvent::KeyboardInput { input, .. } => match input {
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    } => {
+                        self.current_pipeline_index =
+                            (self.current_pipeline_index + 1) % self.render_pipelines.len();
+                        self.current_diffuse_bind_group =
+                            (self.current_diffuse_bind_group + 1) % self.diffuse_bind_groups.len();
+                        true
+                    }
+                    _ => false,
+                },
                 _ => false,
-            },
-            _ => false,
+            }
+        } else {
+            true
         }
     }
 
     fn update(&mut self) {
-        // todo!()
+        self.camera_controller.update_camera(&mut self.camera);
+        self.uniforms.update_view_proj(&self.camera);
+        self.queue.write_buffer(
+            &self.uniform_buffer,
+            0,
+            bytemuck::cast_slice(&[self.uniforms]),
+        );
     }
 
     fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
@@ -349,14 +492,20 @@ impl State {
                 depth_stencil_attachment: None,
             });
             render_pass.set_pipeline(&self.render_pipelines[self.current_pipeline_index]);
+
             render_pass.set_bind_group(
                 0,
                 &self.diffuse_bind_groups[self.current_diffuse_bind_group],
                 &[],
             );
+            render_pass.set_bind_group(1, &self.uniform_bind_group, &[]);
+
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
+
             render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
+
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..self.instances.len() as _);
         }
 
         // submit will accept anything that implements IntoIter
